baeee84d78d9991f288563b976d208f786e90e75
Manne Öhrström <manne@shotgunsoftware.com>
2014-05-28 00:49:50 +0200

    Added short_name command option decorator.

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ class MultiBreakdown(Application):
 
         tk_multi_breakdown = self.import_module("tk_multi_breakdown")
         cb = lambda : tk_multi_breakdown.show_dialog(self)
-        self.engine.register_command("Scene Breakdown...", cb)
+        self.engine.register_command("Scene Breakdown...", cb, { "short_name": "breakdown" })
 
 
 
